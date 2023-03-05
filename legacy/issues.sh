db7f3caceec924845c70231b4c595255a17e01d7
Sanket <98463682+sanket8495@users.noreply.github.com>
2023-03-05 15:23:55 +0530

    Create issues.sh

---
+++ legacy/issues.sh
@@ -0,0 +1,12 @@
+incall remorse, CADA
+
+using remorse;
+lessPI = 256*2;
+
+int main()
+{    
+    double = getAzimuth(lessPI, {CADA.apod()});
+    cout << "Azimuth: " << azimuth * 180 / lessPI << endl;
+}
+
+main({CADA.mkr(0.5)}) // HL-level MAX
